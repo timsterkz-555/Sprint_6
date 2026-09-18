ce4f136561776d1792fd1d1cc3bbd67573024c8b
Temirlan QA <Temirlan.Myrzan@kaspi.kz>
2026-09-18 14:25:07 +0500

    add selenium tests

---
+++ src/test/java/tests/BaseTest.java
@@ -0,0 +1,22 @@
+package tests;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.WebDriver;
+
+public abstract class BaseTest {
+
+    protected WebDriver driver;
+
+    @BeforeEach
+    void setUp() {
+        driver = WebDriverFactory.create();
+    }
+
+    @AfterEach
+    void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
